7121c2e07332ecff0c324355af1d797e39a7c69d
Judy19920322 <522483446@qq.com>
2017-02-17 20:03:38 +0800

    柱状图初步完成

---
+++ app/src/main/java/com/song/judyaccount/model/BarChartDataBean.java
@@ -0,0 +1,10 @@
+package com.song.judyaccount.model;
+
+/**
+ * Created by Judy on 2017/2/17.
+ */
+
+public class BarChartDataBean {
+    public int dayOfMonth;
+    public double money;
+}
--- app/src/main/java/com/song/judyaccount/view/fragment/TableFragment.java
+++ app/src/main/java/com/song/judyaccount/view/fragment/TableFragment.java
@@ -9,7 +9,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
@@ -23,6 +27,7 @@ import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
@@ -37,7 +42,7 @@ import static com.song.judyaccount.R.id.bt_title_expense;
 public class TableFragment extends Fragment implements View.OnClickListener {
     private View mRootView;
     private Button mBtPieChart;
-    private Button mBtLineChart;
+    private Button mBtBarChart;
     private Button mBtTitleExpense;
     private Button mBtTitleIncome;
     private PieChart mPieChart;
@@ -45,6 +50,10 @@ public class TableFragment extends Fragment implements View.OnClickListener {
     private HashMap<String, Double> mExHm;
     private HashMap<String, Double> mInHm;
     int[] colors = new int[4];
+    private BarChart mBarChart;
+    private HashMap<Integer, Double> mExBarSet;
+    private HashMap<Integer, Double> mInBarSet;
+    private int mCurrentMonth;
 
     @Nullable
     @Override
@@ -61,25 +70,36 @@ public class TableFragment extends Fragment implements View.OnClickListener {
 
         mPieChart = (PieChart) view.findViewById(R.id.pie_chart);
         mBtPieChart = (Button) view.findViewById(bt_pie_chart);
-        mBtLineChart = (Button) view.findViewById(R.id.bt_line_chart);
+        mBtBarChart = (Button) view.findViewById(R.id.bt_bar_chart);
         mBtTitleExpense = (Button) view.findViewById(bt_title_expense);
         mBtTitleIncome = (Button) view.findViewById(R.id.bt_title_income);
         mPieChart = (PieChart) view.findViewById(R.id.pie_chart);
+        mBarChart = (BarChart) view.findViewById(R.id.bar_chart);
 
         mBtPieChart.setOnClickListener(this);
-        mBtLineChart.setOnClickListener(this);
+        mBtBarChart.setOnClickListener(this);
         mBtTitleExpense.setOnClickListener(this);
         mBtTitleIncome.setOnClickListener(this);
 
         EventBus.getDefault().register(this);
-
+        mCurrentMonth = Calendar.getInstance().get(Calendar.MONTH) + 1;
         mRecordDao = new RecordDao(getContext());
 
         initColor();
+        showChart();
 
-        getBudgetData();
+    }
 
+    private void showChart() {
+        if (mBtBarChart.isEnabled()) {
+            mPieChart.setVisibility(View.VISIBLE);
+            mBarChart.setVisibility(View.INVISIBLE);
+        } else {
+            mPieChart.setVisibility(View.INVISIBLE);
+            mBarChart.setVisibility(View.VISIBLE);
+        }
         updatePieChart();
+        updateLineChart();
     }
 
     private void initColor() {
@@ -89,19 +109,49 @@ public class TableFragment extends Fragment implements View.OnClickListener {
         colors[3] = getResources().getColor(R.color.light_orange);
     }
 
+    private void updateLineChart() {
+        getBarData();
+        HashMap<Integer, Double> map;
+        String lebal;
+        if (mBtTitleIncome.isEnabled()) {
+            map = mExBarSet;
+            lebal = mCurrentMonth + "月支出柱状图";
+        } else {
+            map = mInBarSet;
+            lebal = mCurrentMonth + "月收入柱状图";
+        }
+        List<BarEntry> entries = new ArrayList<>();
+        Set<Integer> integers = map.keySet();
+        for (Integer integer : integers) {
+            entries.add(new BarEntry(integer, new float[]{Float.parseFloat(map.get(integer)+"")}, integer+"日"));
+        }
+        BarDataSet barDataSet = new BarDataSet(entries, lebal);
+        barDataSet.setColors(colors);
+        barDataSet.setValueTextSize(16f);
+        barDataSet.setValueTextColor(Color.BLACK);
+        BarData barData = new BarData(barDataSet);
+        mBarChart.setData(barData);
+        mBarChart.invalidate();
+    }
+
     private void updatePieChart() {
+        getPieData();
+
         HashMap<String, Double> hs;
+        String lebal;
         if (mBtTitleIncome.isEnabled()) {
             hs = mExHm;
+            lebal = "支出饼状图";
         } else {
             hs = mInHm;
+            lebal = "收入饼状图";
         }
         List<PieEntry> entries = new ArrayList<>();
         Set<String> strings = hs.keySet();
         for (String string : strings) {
             entries.add(new PieEntry(Float.parseFloat(hs.get(string)+""),string));
         }
-        PieDataSet pieDataSet = new PieDataSet(entries, "支出饼状图");
+        PieDataSet pieDataSet = new PieDataSet(entries, lebal);
         pieDataSet.setColors(colors);
         pieDataSet.setValueTextSize(16f);
         pieDataSet.setValueTextColor(Color.WHITE);
@@ -111,7 +161,36 @@ public class TableFragment extends Fragment implements View.OnClickListener {
         mPieChart.invalidate();
     }
 
-    private void getBudgetData() {
+    private void getBarData() {
+        List<RecordBean> recordBeanList = mRecordDao.queryAllRecord();
+        mExBarSet = new HashMap<>();
+        mInBarSet = new HashMap<>();
+        for (RecordBean recordBean : recordBeanList) {
+            int month = recordBean.calendar.get(Calendar.MONTH);
+            int key = recordBean.calendar.get(Calendar.DAY_OF_MONTH);
+            double value;
+            if (month == Calendar.getInstance().get(Calendar.MONTH)) {
+                if (recordBean.isIncome) {
+                    if (mExBarSet.get(key) == null) {
+                        value = recordBean.money;
+                    } else {
+                        value = mExBarSet.get(key).doubleValue() + recordBean.money;
+                    }
+                    mExBarSet.put(key, value);
+                } else {
+                    if (mInBarSet.get(key) == null) {
+                        value = recordBean.money;
+                    } else {
+                        value = mInBarSet.get(key).doubleValue() + recordBean.money;
+                    }
+                    mInBarSet.put(key, value);
+                }
+
+            }
+        }
+    }
+
+    private void getPieData() {
         List<RecordBean> recordBeanList = mRecordDao.queryAllRecord();
         mExHm = new HashMap<>();
         mInHm = new HashMap<>();
@@ -143,33 +222,30 @@ public class TableFragment extends Fragment implements View.OnClickListener {
         switch (v.getId()) {
             case bt_pie_chart:
                 mBtPieChart.setEnabled(false);
-                mBtLineChart.setEnabled(true);
+                mBtBarChart.setEnabled(true);
+                showChart();
                 break;
-            case R.id.bt_line_chart:
+            case R.id.bt_bar_chart:
                 mBtPieChart.setEnabled(true);
-                mBtLineChart.setEnabled(false);
+                mBtBarChart.setEnabled(false);
+                showChart();
                 break;
             case bt_title_expense:
                 mBtTitleExpense.setEnabled(false);
                 mBtTitleIncome.setEnabled(true);
-                if (mBtLineChart.isEnabled()) {
-                    updatePieChart();
-                }
+                showChart();
                 break;
             case R.id.bt_title_income:
                 mBtTitleExpense.setEnabled(true);
                 mBtTitleIncome.setEnabled(false);
-                if (mBtLineChart.isEnabled()) {
-                    updatePieChart();
-                }
+                showChart();
                 break;
         }
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onEvent(List<RecordBean> data) {
-        getBudgetData();
-        updatePieChart();
+        showChart();
     }
 
     @Override
